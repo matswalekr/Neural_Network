f063dcafe0386cb0887d559817c6b0c2f8be7dfb
Mats Walker <walma697@gmail.com>
2025-02-04 18:28:27 +0200

    Remove ignored files and update .gitignore

--- Neural_Network/code/gradient_descent/gradient_descent.h
+++
@@ -1,9 +0,0 @@
-/*
-Header fle for the gradient descent file.
-Includes neurons.h for the neuron** input */
-
-#include "../neurons/neurons.h"
-
-void gradient_descent_neuron(neuron*);
-
-void gradient_descent(neuron**);
--- Neural_Network/code/main/test/test_main.h
+++
@@ -1,4 +0,0 @@
-#ifndef TEST_MAIN_H
-#define TEST_MAIN_H
-
-#endif // TEST_MAIN_H
